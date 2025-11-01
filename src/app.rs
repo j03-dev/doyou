7de95a658b36530e8c6a69da892d9964841c00cf
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2025-11-02 01:28:14 +0300

    just add some svg

--- src/app.rs
+++ src/app.rs
@@ -88,6 +88,12 @@ pub fn App() -> impl IntoView {
 									{ move || item.snippet.channel_title.clone() }
 								</div>
 							    <p class="list-col-wrap text-xs"> { move || item.snippet.description.clone() } </p>
+							    <button class="btn btn-square btn-ghost">
+									<svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+								</button>
+								<button class="btn btn-square btn-ghost">
+								  <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+								</button>
 							</div>
 						</li>
 					}
