a8866781dfa3a50dba078a47df4999d3ba2cadd1
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2025-11-07 20:19:27 +0300

    still not work

--- src-oxapy/main.py
+++ src-oxapy/main.py
@@ -4,25 +4,53 @@ from oxapy import (
     Cors,
     Router,
     FileStreaming,
-    get,
     exceptions,
 )
 from googleapiclient.discovery import build
 
+import contextlib
 import dotenv
 import os
 import yt_dlp
 import logging
+import shutil
+import tempfile
 
 dotenv.load_dotenv()
 
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 MEDIA_DIR = "media"
-
+SECRET_FILE = "/etc/secrets/cookies.txt"
 youtube = build("youtube", "v3", developerKey=GOOGLE_API_KEY)
 
 
+@contextlib.contextmanager
+def temp_cookie_file(original_path):
+    if not os.path.exists(original_path):
+        yield None
+        return
+
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        temp_path = os.path.join(tmp_dir, "cookies.txt")
+        shutil.copy(original_path, temp_path)
+        yield temp_path
+    finally:
+        if tmp_dir:
+            shutil.rmtree(tmp_dir)
+
+
+def log(r: Request, next, **kwargs):
+    logging.log(1000, f"{r.method} {r.uri}")
+    return next(r, **kwargs)
+
+
+router = Router()
+router.middleware(log)
+
+
+@router.get("/api/v1/search")
 def search_youtube(r: Request):
     q = r.query()
     query_searh = q.get("q")
@@ -43,6 +71,7 @@ def search_youtube(r: Request):
         raise exceptions.InternalError(f"Verify your internet connection: {e}")
 
 
+@router.get("/api/v1/download")
 def download(r: Request):
     q = r.query()
     video_id = q.get("id")
@@ -56,24 +85,29 @@ def download(r: Request):
         return {"video_id": video_id}
 
     url = f"https://www.youtube.com/watch?v={video_id}"
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-        "outtmpl": f"{path_part}.%(ext)s",
-        "noplaylist": True,
-    }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    with temp_cookie_file(SECRET_FILE) as cookie_file:
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "outtmpl": f"{path_part}.%(ext)s",
+            "noplaylist": True,
+        }
+        if cookie_file:
+            ydl_opts["cookiefile"] = cookie_file
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
     return {"video_id": video_id}
 
 
+@router.get("/api/v1/listen")
 def listen(r: Request):
     q = r.query()
     video_id = q.get("id")
@@ -82,25 +116,11 @@ def listen(r: Request):
     return FileStreaming(path, content_type="audio/mpeg")
 
 
-def log(r: Request, next, **kwargs):
-    logging.log(1000, f"{r.method} {r.uri}")
-    return next(r, **kwargs)
-
-
 def main():
-    (
-        HttpServer(("0.0.0.0", 8080))
-        .cors(Cors())
-        .attach(
-            Router()
-            .middleware(log)
-            .route(get("/health", lambda _r: "Good!"))
-            .route(get("/api/v1/search", search_youtube))
-            .route(get("/api/v1/download", download))
-            .route(get("/api/v1/listen", listen))
-        )
-        .run()
-    )
+    server = HttpServer(("0.0.0.0", 8080))
+    server.cors(Cors())
+    server.attach(router)
+    server.run()
 
 
 if __name__ == "__main__":
