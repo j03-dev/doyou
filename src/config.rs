2a8910cbc06251f8a9c88773d35a6414852634aa
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-02-11 23:26:57 +0300

    rename some variable on config.rs

--- src/config.rs
+++ src/config.rs
@@ -34,21 +34,31 @@ fn get_config_path() -> Result<String, Error> {
     Ok(format!("{base_dir}/config.json"))
 }
 
+/// Gets the Android application's files directory path.
+/// This is typically `/data/data/<package_name>/files/`
 fn get_android_files_dir() -> Result<String, Error> {
     let android_context = ndk_context::android_context();
     let java_vm = unsafe { jni::JavaVM::from_raw(android_context.vm().cast()) }?;
-    let mut java = java_vm.attach_current_thread()?;
-    let fs = unsafe { jni::objects::JObject::from_raw(android_context.context().cast()) };
+    let mut jni_env = java_vm.attach_current_thread()?;
 
-    let files_dir = java
-        .call_method(fs, "getFilesDir", "()Ljava/io/File;", &[])?
+    let android_context_object =
+        unsafe { jni::objects::JObject::from_raw(android_context.context().cast()) };
+
+    let java_file_object = jni_env
+        .call_method(
+            android_context_object,
+            "getFilesDir",
+            "()Ljava/io/File;",
+            &[],
+        )?
         .l()?;
 
-    let files_dir: jni::objects::JString = java
-        .call_method(&files_dir, "toString", "()Ljava/lang/String;", &[])?
+    let path_as_jstring: jni::objects::JString = jni_env
+        .call_method(&java_file_object, "toString", "()Ljava/lang/String;", &[])?
         .l()?
         .try_into()?;
 
-    let files_dir = java.get_string(&files_dir)?;
-    Ok(files_dir.to_str()?.to_string())
+    let path_as_java_str = jni_env.get_string(&path_as_jstring)?;
+
+    Ok(path_as_java_str.to_str()?.to_string())
 }
